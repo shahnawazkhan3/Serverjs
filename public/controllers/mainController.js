f19999413c34b3d33e58371d9cbb9aecba6ec7c5
shahnawazGold <shahnawazkhan_333@yahoo.com>
2018-01-15 14:52:04 +0500

    last update

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -26,7 +26,10 @@ var mainApp = angular.module ('workshop', ['ngRoute']);
                templateUrl: 'pages/about.html',
                controller: 'aboutController'
             }).
-            
+             when('/contact', {
+               templateUrl: 'pages/contact.html',
+               controller: 'contactController'
+            }).
             otherwise({
                redirectTo: '/'
             });
@@ -62,6 +65,32 @@ mainApp.controller('aboutController',function ($scope) {
    console.log("about");
 
 
+
+     
+});
+
+
+
+mainApp.controller('contactController',function ($scope) {
+   console.log("contactController");
+
+$scope.submit =function() {
+                // $scope.shah =studentSetup.studenOne.admission;
+                console.log("here controler send requst addContact shah");
+                console.log($scope.admission);
+
+    //     $http.post('/home',$scope.admission).success(function(response) {
+    //     console.log(response);
+    //     console.log("http here");
+    //     // $location.path("/");
+    //     //    $window.location.href = '/';
+    //     //  alert('Add data in Patient');
+    // });
+    
+        
+        //$scope.contactsArray.push();//pushing your object here     
+
+    };
 
      
 });
